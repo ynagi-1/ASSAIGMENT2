1455583ba5381eb7c1a6cc8908647b3ad5aedd66
user <sailaubekzhangir@gmail.com>
2025-01-10 20:56:44 +0500

    Ex

---
+++ src/Task1.java
@@ -0,0 +1,14 @@
+public class Task1 {
+    public static void main(String[] args) {
+
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println("intArray[" + i + "] = " + intArray[i]);
+        }
+        for (int i = 0; i < doubleArray.length; i++) {
+            System.out.println("doubleArray[" + i + "] = " + doubleArray[i]);
+        }
+    }
+}
--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,30 @@
 public class Task2 {
+        public static final String ODD = "Odd";
+        public static final String EVEN = "Even";
+        public static String[] strings = new String[5];
+
+        public static void main(String[] args) {
+            for (int i = 0; i < strings.length; i++) {
+                if (i % 2 == 0) {
+                    strings[i] = EVEN;
+                } else {
+                    strings[i] = ODD;
+                }
+            }
+
+            // Вывод значений массива (для проверки)
+            System.out.print("index = 0");
+            System.out.println(" value = " + strings[0]);
+            System.out.print("index = 1");
+            System.out.println(" value = " + strings[1]);
+            System.out.print("index = 2");
+            System.out.println(" value = " + strings[2]);
+            System.out.print("index = 3");
+            System.out.println(" value = " + strings[3]);
+            System.out.print("index = 4");
+            System.out.println(" value = " + strings[4]);
+        }
+
+
+
 }
