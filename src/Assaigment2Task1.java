342dd9fd670aac48796aadff580ed5129e4e5ee4
user <sailaubekzhangir@gmail.com>
2025-01-11 12:00:20 +0500

    EXERsice

--- src/Assaigment2Task1.java
+++
@@ -1,2 +0,0 @@
-public class Assaigment2Task1 {
-}
--- src/Assaigment2_2_Task1.java
+++ src/Assaigment2_2_Task1.java
@@ -1,2 +1,27 @@
 public class Assaigment2_2_Task1 {
+    public static void main(String[] args) {
+        printHydrogenInfo();
+        printJavaInfo();
+    }
+
+    public static void printHydrogenInfo() {
+        System.out.println("Hydrogen:");
+        System.out.println("This is a chemical element of the periodic system with the designation H and atomic number 1.");
+        System.out.println("Is the lightest element in the table.");
+        System.out.println("Hydrogen is used in:");
+        System.out.println("-Chemical industry;");
+        System.out.println("-Oil refining industry;");
+        System.out.println("-Aviation;");
+        System.out.println("-Electricity.");
+    }
+
+    public static void printJavaInfo() {
+        System.out.println("Java Island:");
+        System.out.println("Included in Indonesia.");
+        System.out.println("The states of Mataram, Majapahit, Demak originated on the island.");
+        System.out.println("Java is the most populated island in the world:");
+        System.out.println("The total population is 140 million people.");
+        System.out.println("Population density - 1061 people/km²");
+        System.out.println("One of the famous varieties of coffee, Kopi Luwak, is produced here.");
+    }
 }
